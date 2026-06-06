682b7a329fa55f1ce0f0f9cc3217c08af260fd4c
Rithanya Rozario <rithanya.rozario@gmail.com>
2026-06-06 16:32:01 +1000

    access to register and login page seperatley

---
+++ .history/project/app_20260606155824.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+from main import login, register
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return render_template("login.html")
+
+@app.route("/", methods=["GET", "POST"])
+def home():
+
+    if request.method == "POST":
+        username = request.form["username"]
+        password = request.form["password"]
+
+        result=login(username,password)
+        return result
+    return render_template("login.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+    
